3aa51668ba9bd6b93fde08a30b5221b5f2bc5d46
Ali Hassan <engineralihassan@gmail.com>
2023-06-14 12:06:50 +0500

    ViewComponents is continue to learning

---
+++ Mvc Part 2/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc_Part_2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        [Route("home")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
